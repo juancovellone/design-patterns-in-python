38628fff64a555fca4eb37db020964846517ae4f
Juan <jcovellone@navent.com>
2022-02-17 10:07:36 -0300

    Se le agrega la denominación Strategy a las clases de la estrategia

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 logger = logging.getLogger(__name__)
 
 
-class Operation(ABC):
+class OperationStrategy(ABC):
     """
     Abstract Base Class
     """
@@ -15,25 +15,25 @@ class Operation(ABC):
         pass
 
 
-class SumOperation(Operation):
+class SumOperationStrategy(OperationStrategy):
 
     def calculate(self, number_1, number_2):
         return number_1 + number_2
 
 
-class SubtractionOperation(Operation):
+class SubtractionOperationStrategy(OperationStrategy):
 
     def calculate(self, number_1, number_2):
         return number_1 - number_2
 
 
-class MultiplicationOperation(Operation):
+class MultiplicationOperationStrategy(OperationStrategy):
 
     def calculate(self, number_1, number_2):
         return number_1 * number_2
 
 
-class DivisionOperation(Operation):
+class DivisionOperationStrategy(OperationStrategy):
 
     def calculate(self, number_1, number_2):
         try:
@@ -43,12 +43,12 @@ class DivisionOperation(Operation):
             logger.exception(e)
 
 
-class CalculatorStrategy:
+class Calculator:
     OPERATION_TYPE = {
-        'sum': SumOperation(),
-        'subtraction': SubtractionOperation(),
-        'multiplication': MultiplicationOperation(),
-        'division': DivisionOperation()
+        'sum': SumOperationStrategy(),
+        'subtraction': SubtractionOperationStrategy(),
+        'multiplication': MultiplicationOperationStrategy(),
+        'division': DivisionOperationStrategy()
     }
 
     def __init__(self):
@@ -56,14 +56,25 @@ class CalculatorStrategy:
         self.__last_result = None
 
     def calculate(self, number_1, number_2):
+        """
+        Performs the calculation according to the chosen operation.
+        :return: None
+        """
         if self.__operation is not None:
             self.__last_result = self.__operation.calculate(number_1, number_2)
             return self.__last_result
 
     def get_last_result(self):
+        """
+        Returns the value of the last calculated operation.
+        :return: Int
+        """
         return self.__last_result
 
     def set_operation(self, operation_type):
+        """
+        Configures the operation to be performed to calculate.
+        :return: None"""
         try:
             self.__operation = self.OPERATION_TYPE[operation_type]
         except KeyError:
@@ -71,30 +82,19 @@ class CalculatorStrategy:
             logger.error(f"The {operation_type} operation can't be resolved.")
 
     def calculate_operation(self, operation_type, number_1, number_2):
+        """
+        Perform the calculation of the operation.
+        :return: int
+        """
         self.set_operation(operation_type)
         return self.calculate(number_1, number_2)
 
 
 if __name__ == '__main__':
-    calculator = CalculatorStrategy()
+    calculator = Calculator()
     number_1 = 15
     number_2 = 5
-    operation = 'sum'
-    print(f'The result of {operation} operation between {number_1} and {number_2} '
-          f'is: {calculator.calculate_operation(operation, number_1, number_2)}.')
-
-    operation = 'subtraction'
-    print(f'The result of {operation} operation between {number_1} and {number_2} '
-          f'is: {calculator.calculate_operation(operation, number_1, number_2)}.')
-
-    operation = 'multiplication'
-    print(f'The result of {operation} operation between {number_1} and {number_2} '
-          f'is: {calculator.calculate_operation(operation, number_1, number_2)}.')
-
-    operation = 'division'
-    print(f'The result of {operation} operation between {number_1} and {number_2} '
-          f'is: {calculator.calculate_operation(operation, number_1, number_2)}.')
-
-    operation = 'logarithm'
-    print(f'The result of {operation} operation between {number_1} and {number_2} '
-          f'is: {calculator.calculate_operation(operation, number_1, number_2)}.')
+    operations = ['sum', 'subtraction', 'multiplication', 'division']
+    for operation in operations:
+        print(f'The result of {operation} operation between {number_1} and {number_2} '
+              f'is: {calculator.calculate_operation(operation, number_1, number_2)}.')
