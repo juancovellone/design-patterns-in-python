31493e1cfc5cdf5d443b62d2d49dae664f6867c6
Juan Covellone <jcovellone@navent.com>
2022-10-07 12:31:59 -0300

    se cambia set_operation por decorador

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -11,33 +11,33 @@ class OperationStrategy(ABC):
     """
 
     @abstractmethod
-    def calculate(self, number_1, number_2):
+    def calculate(self, number1, number2):
         pass
 
 
 class SumOperationStrategy(OperationStrategy):
 
-    def calculate(self, number_1, number_2):
-        return number_1 + number_2
+    def calculate(self, number1, number2):
+        return number1 + number2
 
 
 class SubtractionOperationStrategy(OperationStrategy):
 
-    def calculate(self, number_1, number_2):
-        return number_1 - number_2
+    def calculate(self, number1, number2):
+        return number1 - number2
 
 
 class MultiplicationOperationStrategy(OperationStrategy):
 
-    def calculate(self, number_1, number_2):
-        return number_1 * number_2
+    def calculate(self, number1, number2):
+        return number1 * number2
 
 
 class DivisionOperationStrategy(OperationStrategy):
 
-    def calculate(self, number_1, number_2):
+    def calculate(self, number1, number2):
         try:
-            return number_1 / number_2
+            return number1 / number2
         except ZeroDivisionError as e:
             logger.error("You can't divide by zero.")
             logger.exception(e)
@@ -55,46 +55,53 @@ class Calculator:
         self.__operation = None
         self.__last_result = None
 
-    def calculate(self, number_1, number_2):
+    def calculate(self, number1, number2):
         """
         Performs the calculation according to the chosen operation.
         :return: None
         """
         if self.__operation is not None:
-            self.__last_result = self.__operation.calculate(number_1, number_2)
+            self.__last_result = self.__operation.calculate(number1, number2)
             return self.__last_result
 
-    def get_last_result(self):
+    @property
+    def last_result(self):
         """
         Returns the value of the last calculated operation.
         :return: Int
         """
         return self.__last_result
 
-    def set_operation(self, operation_type):
+    @property
+    def operation(self):
+        return self.__operation
+
+    @operation.setter
+    def operation(self, operation_type):
         """
         Configures the operation to be performed to calculate.
-        :return: None"""
+        :return: None
+        """
         try:
             self.__operation = self.OPERATION_TYPE[operation_type]
         except KeyError:
             self.__operation = None
             logger.error(f"The {operation_type} operation can't be resolved.")
 
-    def calculate_operation(self, operation_type, number_1, number_2):
+    def calculate_operation(self, operation_type, number1, number2):
         """
         Perform the calculation of the operation.
         :return: int
         """
-        self.set_operation(operation_type)
-        return self.calculate(number_1, number_2)
+        self.operation = operation_type
+        return self.calculate(number1, number2)
 
 
 if __name__ == '__main__':
     calculator = Calculator()
-    number_1 = 15
-    number_2 = 5
+    number1 = 15
+    number2 = 5
     operations = ['sum', 'subtraction', 'multiplication', 'division']
     for operation in operations:
-        print(f'The result of {operation} operation between {number_1} and {number_2} '
-              f'is: {calculator.calculate_operation(operation, number_1, number_2)}.')
+        print(f'The result of {operation} operation between {number1} and {number2} '
+              f'is: {calculator.calculate_operation(operation, number1, number2)}.')
